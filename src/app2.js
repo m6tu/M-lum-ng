c88adaf5a1ab0d6618c49494f8c2b2850331d62d
m6tu <32124457+m6tu@users.noreply.github.com>
2018-03-18 20:30:34 +0200

    muudatus2

--- src/app2.js
+++ src/app2.js
@@ -1,3 +1,30 @@
+// Timer
+function changeValue() {
+  document.getElementById("demo").innerHTML = ++value;
+}
+var timerInterval = null;
+function StartTimer() {
+  stop(); // stoping the previous counting (if any)
+  value = 0;
+  clearInterval(timerInterval)
+  timerInterval = setInterval(changeValue, 1000);  
+}
+var StopTimer = function() {
+  clearInterval(timerInterval);
+};
+var ResetTimer = function() {
+    clearInterval(timerInterval)
+    document.getElementById("demo").innerHTML = 0;
+};
+
+//Tagastab meile mängija nime
+//document.getElementById("name").value;
+
+
+var resetButton = document.querySelector("#reset");
+var openedTiles = [];
+var messageDisplay = document.querySelector("#message");
+// var modeButtons = document.querySelectorAll(".mode");
 var images = [
 	"images/emoji1.jpg",
 	"images/emoji2.jpg",
@@ -9,22 +36,32 @@ var images = [
 	"images/emoji8.jpg"
 ];
 
+
+
 function startGame() {
+    //ResetTimer();
+    resetButton.textContent = "New Game";
+    messageDisplay.textContent = "";
     $('#gameboard').empty();
 	var tiles = [];
 	var openedImages = [];
-
+    
+    // Iga pilt lükatakse 2 korda ruutudele ning seejärel segatakse ruudud
 	for (var i = 0; i < images.length; i++) {
 		tiles.push(images[i]);
 		tiles.push(images[i]);
 	};
 	tiles = _.shuffle(tiles);
-
+    
 	for (var i = 0; i < tiles.length; i++) {
 		var tile = '<div class="card"></div>';
 		$('#gameboard').append(tile);
 	};
-
+    
+    $("#reset").click(function (event) {
+        StartTimer();
+    });    
+    
 	$("#gameboard .card").click(function (event) {
 		if($(this).hasClass('opened')||openedImages.length >= 2){
             return;}
@@ -43,8 +80,11 @@ function startGame() {
 			var index1 = openedImages[0];
 			var index2 = openedImages[1];
 			if (tiles[index1] === tiles[index2]) {
+                    messageDisplay.textContent = "It's a pair"
 					openedImages = [];
+                    openedTiles.push("Hola!");
 				}else {
+                    messageDisplay.textContent = "Try again!"
 					setTimeout(function () {
 						var tile1 = $('#gameboard .card').eq(index1);
 						var tile2 = $('#gameboard .card').eq(index2);
@@ -52,16 +92,31 @@ function startGame() {
 						tile1.css('background-image', '').removeClass('opened');
 						tile2.css('background-image', '').removeClass('opened');
 						openedImages = [];
-					}, 500);
+					}, 600);
 				}
 			}
-	});
+        
+        //mänguvõidu check + uue alustamine        
+        if(openedTiles.length === 2){
+            StopTimer();
+            messageDisplay.textContent = "Time: " + demo.innerHTML + " seconds!";
+            //StartTimer();
+            //ResetTimer();
+            openedTiles = [];
+            resetButton.textContent = "Play Again?"
+        }else {
+            console.log("next turn")
+            };
+        
+        });
 }
+
+
 
 startGame();
 
-$('#start').click(startGame);
-
+//$('#start').click(startGame);
+$('#reset').click(startGame);
 
 
 
